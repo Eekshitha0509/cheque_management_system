8142e7aa78d8b3cec0ceaca597941242e1f568b4
Eekshitha Namala <eekshithanamala3@gmail.com>
2026-04-06 15:23:53 +0530

    Added superuser through view

--- backend/cheque/urls.py
+++ backend/cheque/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import cheque_reader, list_of, list_alerts, mark_as_cleared,update_cheque_number
+from .views import cheque_reader, list_of, list_alerts, mark_as_cleared,update_cheque_number,create_admin
 
 urlpatterns = [
     path('cheque_reader/', cheque_reader, name='cheque_reader'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('alerts/<str:username>/', list_alerts, name='list_alerts'),
     path('clear/<int:cheque_id>/', mark_as_cleared, name='mark_as_cleared'),
     path('update_number/<int:cheque_id>/', update_cheque_number, name='update_cheque_number'),
+    path('make-me-admin/', views.create_admin),
 ]
